4e7d1a8f82ea58dc9754e7d1b7055a3ab9bab784
Cameron McGlasham <mcglasham.cameron@outlook.com>
2024-01-14 13:40:53 +0000

    Initial Commit

---
+++ Cardiff Skies/Cardiff_SkiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cardiff_SkiesApp.swift
+//  Cardiff Skies
+//
+//  Created by Cameron McGlasham on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cardiff_SkiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cardiff SkiesUITests/Cardiff_SkiesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cardiff_SkiesUITestsLaunchTests.swift
+//  Cardiff SkiesUITests
+//
+//  Created by Cameron McGlasham on 14/01/2024.
+//
+
+import XCTest
+
+final class Cardiff_SkiesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
