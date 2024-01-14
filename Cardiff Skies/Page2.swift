7d52d34d9c8254142b2ab7467d1ab978bdc21c52
Cameron McGlasham <mcglasham.cameron@outlook.com>
2024-01-14 13:49:26 +0000

    Made a second view

---
+++ Cardiff Skies/Page2.swift
@@ -0,0 +1,20 @@
+//
+//  Page2.swift
+//  Cardiff Skies
+//
+//  Created by Cameron McGlasham on 14/01/2024.
+//
+
+import SwiftUI
+
+struct Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page2_Previews: PreviewProvider {
+    static var previews: some View {
+        Page2()
+    }
+}
